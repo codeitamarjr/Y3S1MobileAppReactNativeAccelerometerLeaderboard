7c7839cf91b22aecaae8e65d3f220c7ba49e1a94
Itamar Junior <itjunior@gmail.com>
2022-12-08 13:47:28 +0000

    accelerometer

--- screens/AccelerometerScreen.js
+++ screens/AccelerometerScreen.js
@@ -1,10 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { Accelerometer } from 'expo-sensors';
+
 
 const AccelerometerScreen = () => {
+
+    const [{ x, y, z }, setData] = useState({
+        x: 0,
+        y: 0,
+        z: 0,
+    });
+    const [subscription, setSubscription] = useState(null);
+
+    const _slow = () => Accelerometer.setUpdateInterval(1000);
+    const _fast = () => Accelerometer.setUpdateInterval(16);
+
+    const _subscribe = () => {
+        setSubscription(
+            Accelerometer.addListener(setData)
+        );
+    };
+
+    const _unsubscribe = () => {
+        subscription && subscription.remove();
+        setSubscription(null);
+    };
+
+    useEffect(() => {
+        _subscribe();
+        return () => _unsubscribe();
+    }, []);
+
+
+
     return (
-        <View>
-            <Text>Accelerometer Screen</Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>Accelerometer: (in gs where 1g = 9.81 m/s^2)</Text>
+            <Text style={styles.text}>x: {x}</Text>
+            <Text style={styles.text}>y: {y}</Text>
+            <Text style={styles.text}>z: {z}</Text>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} style={styles.button}>
+                    <Text>{subscription ? 'On' : 'Off'}</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={_slow} style={[styles.button, styles.middleButton]}>
+                    <Text>Slow</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={_fast} style={styles.button}>
+                    <Text>Fast</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
